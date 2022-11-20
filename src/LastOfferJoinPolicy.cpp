81059828ce8bbef32641ae6fd347296c62ab041b
yuvalmek <yuvalmek@post.bgu.ac.il>
2022-11-20 23:26:03 +0200

    Update LastOfferJoinPolicy.cpp

--- src/LastOfferJoinPolicy.cpp
+++ src/LastOfferJoinPolicy.cpp
@@ -1,7 +1,7 @@
 #include "../include/JoinPolicy.h"
-#include "../include/Party.h"
 #include "../include/Agent.h"
 
-void LastOfferJoinPolicy::join(vector<Offer> offs, Party &p) {
-    
+void LastOfferJoinPolicy::join(vector<int> &offs, vector<vector<int>> &partiesBy, Graph &g) {
+    // the method selects the coalition that made the last offer
+    return offs[offs.size() - 1];
 }
