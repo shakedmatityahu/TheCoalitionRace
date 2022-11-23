902e39ce22a4968519244183045d76a9033cadeb
yuvalmek <yuvalmek@post.bgu.ac.il>
2022-11-23 16:59:57 +0200

    fix place for sim init

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,8 @@ int main(int argc, char **argv)
     Simulation simulation = Parser::readSimulation(argv[1]);
 
     // run simulation and store json state after each iteration
-    vector<json> outPerIter = {Parser::makeJson(simulation)};
     simulation.init();
+    vector<json> outPerIter = {Parser::makeJson(simulation)};
     int simNum = 0;
     while (!simulation.shouldTerminate())
     {
